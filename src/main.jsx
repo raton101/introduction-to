bf717bdd42ea6eff08d457a3cb0d6e7177ee1149
unknown <ratonbci95@gmail.com>
2023-09-20 00:00:35 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello from react router!!</div>,
+  },
+  {
+    path: "/about",
+    element:<div>hii i am a afran raton</div>
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>,
+)
